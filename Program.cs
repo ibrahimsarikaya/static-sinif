05e97a1f67218ba53ce263309b12ce2743fab310
ibrahimsarikaya <ibo7534@gmail.com>
2022-06-15 18:11:55 +0300

    Codding

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ namespace static_sinif
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi);
+            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi); // CalisanSayisi ilk olarak 0 olduğu için çıktısı > "Çalışan Sayısı: 0"
 
-            Calisan calisan = new Calisan("Ayşe","Yılmaz","IK");
-            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi);
-            Calisan calisan1 = new Calisan("Deniz","Arda","IK");
-            Calisan calisan2 = new Calisan("İbrahim","Sarıkaya","IK");
-            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi);
+            Calisan calisan = new Calisan("Ayşe","Yılmaz","IK"); // İnstance oluşturup isim,soyisim,departmanını ekledik ve ayrıca calisanSayisi++ da burada çalıştı. Çünkü 1 tane çalışan bilgisi eklendi.
+            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi); // Çıktısı: "Çalışan Sayısı: 1"
+            Calisan calisan1 = new Calisan("Deniz","Arda","IK"); // Yine farklı bir instance oluşturup veri ekledik.
+            Calisan calisan2 = new Calisan("İbrahim","Sarıkaya","IK");//Aynı şekilde farklı bir instance oluşturup veri ekledik.
+            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi);//Çıktısı:"Çalışan Sayısı: 3"
 
-            Console.WriteLine("Toplama İşlemi Sonucu: {0}", Islemler.Topla(100,200));
+            Console.WriteLine("Toplama İşlemi Sonucu: {0}", Islemler.Topla(100,200));//Static işlemlerde "Islemler.Topla olarak yazılmaktadır."
             Console.WriteLine("Çıkarma İşlemi Sonucu: {0}", Islemler.Cıkar(400,50));
 
         }
@@ -29,14 +29,14 @@ namespace static_sinif
         private string Soyisim;
         private string Departman;
 
-        static Calisan()
+        static Calisan() 
         {
             calisanSayisi = 0;
         }
 
-        public Calisan(string ısim, string soyisim, string departman)
+        public Calisan(string isims, string soyisim, string departman)
         {
-            this.Isim = ısim;
+            this.Isim = isims;
             this.Soyisim = soyisim;
             this.Departman = departman;
             calisanSayisi ++;
